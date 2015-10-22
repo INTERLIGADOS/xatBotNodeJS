c38c6fc7a3cc4a63852f49ee94f0f6e8c29665f0
Xeta <zoro.fmt@gmail.com>
2015-10-22 06:57:41 +0200

    Some optimizations

--- main.js
+++ main.js
@@ -5,63 +5,63 @@ var bot = new xatNetwork(config);
 
 bot.login(function(){
 
-	bot.connectToChat(function(packet){
+    bot.connectToChat(function(packet){
 
-		var hook = '';
-		var args = packet.elements;;
-		switch(packet.node){
+        var hook = '';
+        var args = packet.elements;;
+        switch(packet.node){
 
-			case 'm':
-				if(args.u && !args.s && !args.p && args.i)
-					hook = 'onMessage';
-			break;
+            case 'm':
+                if(args.u && !args.s && !args.p && args.i)
+                    hook = 'onMessage';
+            break;
 
-			case 'p':
-				if(args.d)
-					hook = 'onPC';
-				else
-					hook = 'onPM';
-			break;
+            case 'p':
+                if(args.d)
+                    hook = 'onPC';
+                else
+                    hook = 'onPM';
+            break;
 
-			case 'z':
-				hook = 'onTickle';
-			break;
+            case 'z':
+                hook = 'onTickle';
+            break;
 
-			case 'l':
-				hook = 'onUserLeft';
-			break;
+            case 'l':
+                hook = 'onUserLeft';
+            break;
 
-			case 'u':
-				hook = 'onUserJoined'
-			break;
+            case 'u':
+                hook = 'onUserJoined'
+            break;
 
-			case 'idle':
-				hook = 'onIdle';
-			break;
+            case 'idle':
+                hook = 'onIdle';
+            break;
 
-			default:
-				console.log('Undefined packet "' + packet.node + '"');
-			break;
+            default:
+                console.log('Undefined packet "' + packet.node + '"');
+            break;
 
-		}
+        }
 
-		if(hook){
+        if(hook){
 
-			if((hook == 'onMessage' || hook == 'onPC' || hook == 'onPM') && args.t.charAt(0) == config.cmdcode){
-				
-				if(hook == 'onMessage')
-					args.type = 1;
-				else if(hook == 'onPM')
-					args.type = 2;
-				else if(hook == 'onPC')
-					args.type = 3;
+            if((hook == 'onMessage' || hook == 'onPC' || hook == 'onPM') && args.t.charAt(0) == config.cmdcode){
 
-				hook = 'onCommand';
-			}
-			var module = require('./modules/' + hook);
-			var obj    = new module(bot, args);
+                if(hook == 'onMessage')
+                    args.type = 1;
+                else if(hook == 'onPM')
+                    args.type = 2;
+                else if(hook == 'onPC')
+                    args.type = 3;
 
-		}
-	});
+                hook = 'onCommand';
+            }
+            var module = require('./modules/' + hook);
+            var obj    = new module(bot, args);
+
+        }
+    });
 
 });
